20924e12de6d4ed14d6aa401b9ed6d0c6db59d76
Grachya Ayvazyan <ayvazyanhrachya54@gmail.com>
2026-03-30 12:24:34 +0300

    Fix: Delete Secrets

---
+++ bot/constants.py
@@ -0,0 +1,12 @@
+API_TOKEN = ""
+
+HEADERS = {
+  "Authorization": "Bearer " + API_TOKEN,
+  "User-Agent" : "STRATZ_API",
+  "Content-Type": "application/json"
+}
+
+GRAPHQL_ENDPOINT = "https://api.stratz.com/graphql"
+
+
+tg_bot_token = ""
